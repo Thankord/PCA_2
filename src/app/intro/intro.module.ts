d7044d4c4af5a5b0a8a412dbd05f4a66d6f4bd9c
Thankord <danedu0727@gmail.com>
2023-07-10 19:00:05 -0500

    Tarea de 06 y 07 de Julio

--- src/app/intro/intro.module.ts
+++ src/app/intro/intro.module.ts
@@ -1,12 +1,11 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
+import { IntroPage } from './intro.page';
 
 import { IntroPageRoutingModule } from './intro-routing.module';
 
-import { IntroPage } from './intro.page';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -10,34 +10,34 @@ export class IntroPage implements OnInit {
 
   slides = [
     {
-      title:"Gatitos",
-      img: "assets/images/gatitos.webp",
+      title:"Batería eléctrica",
+      img: "assets/images/Drums.jpg",
       icon: "beer-outline",
-      description: "Hola soy la explicación del slide"
+      description: "Una batería musical es un conjunto de tambores y platillos que se tocan simultáneamente para proporcionar ritmo y percusión en una canción."
     },
     {
-      title:"Perritos",
-      img: "assets/images/perritos.jpg",
+      title:"Guitarra eléctrica",
+      img: "assets/images/Guitarr.jpg",
       icon: "barbell-outline",
-      description: "Hola soy la explicación del slide"
+      description: "Una guitarra eléctrica convierte las vibraciones de las cuerdas en señales eléctricas para producir un sonido amplificado."
     },
     {
-      title:"Hamsters",
-      img: "assets/images/hamster.jpg",
+      title:"Bajo eléctrico",
+      img: "assets/images/Bass.jpg",
       icon: "barbell-outline",
-      description: "Hola soy la explicación del slide"
+      description: "El bajo eléctrico es un instrumento de cuerda más grande que la guitarra eléctrica, utilizado para tocar líneas de bajo en distintos géneros musicales."
     },
     {
-      title:"Mas gatitos",
-      img: "assets/images/masgatos.jpg",
+      title:"Piano",
+      img: "assets/images/Piano.jpg",
       icon: "barbell-outline",
-      description: "Hola soy la explicación del slide"
+      description: "El piano es un instrumento de teclado que produce sonidos al golpear las cuerdas con martillos."
     },
     {
-      title:"Cocodrilos",
-      img: "assets/images/cocodrilo.jpeg",
+      title:"Saxofón",
+      img: "assets/images/Sax.jpg",
       icon: "barbell-outline",
-      description: "Hola soy un cocodrilo"
+      description: "El saxofón es un instrumento de viento-madera utilizado en varios géneros musicales, conocido por su sonido cálido y expresivo."
     }
   ]
   
