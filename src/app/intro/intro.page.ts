111e3dc7dca4d0eb9a58a740ac17cc2463e60b20
Thankord <danedu0727@gmail.com>
2023-07-10 21:09:15 -0500

    Clase 10 Julio 2023

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {Storage} from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-intro',
@@ -39,15 +40,15 @@ export class IntroPage implements OnInit {
       icon: "barbell-outline",
       description: "El saxofón es un instrumento de viento-madera utilizado en varios géneros musicales, conocido por su sonido cálido y expresivo."
     }
-  ]
+  ]  
   
-  
-  constructor(private router: Router) { }
+  constructor(private router: Router, private storage: Storage) { }
 
   ngOnInit() {
   }
 
   finish(){
+    this.storage.set("introShow", true);
     this.router.navigateByUrl("/home");
     console.log("salir")
 
