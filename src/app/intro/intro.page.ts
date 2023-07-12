54663ce18aa1321521afbb59701a865d430be4ee
Thankord <danedu0727@gmail.com>
2023-07-12 18:33:15 -0500

    Actividad clase del 12 Julio

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -11,31 +11,31 @@ export class IntroPage implements OnInit {
 
   slides = [
     {
-      title:"Batería eléctrica",
+      title:"batería eléctrica",
       img: "assets/images/Drums.jpg",
       icon: "beer-outline",
       description: "Una batería musical es un conjunto de tambores y platillos que se tocan simultáneamente para proporcionar ritmo y percusión en una canción."
     },
     {
-      title:"Guitarra eléctrica",
+      title:"guitarra eléctrica",
       img: "assets/images/Guitarr.jpg",
       icon: "barbell-outline",
       description: "Una guitarra eléctrica convierte las vibraciones de las cuerdas en señales eléctricas para producir un sonido amplificado."
     },
     {
-      title:"Bajo eléctrico",
+      title:"bajo eléctrico",
       img: "assets/images/Bass.jpg",
       icon: "barbell-outline",
       description: "El bajo eléctrico es un instrumento de cuerda más grande que la guitarra eléctrica, utilizado para tocar líneas de bajo en distintos géneros musicales."
     },
     {
-      title:"Piano",
+      title:"piano",
       img: "assets/images/Piano.jpg",
       icon: "barbell-outline",
       description: "El piano es un instrumento de teclado que produce sonidos al golpear las cuerdas con martillos."
     },
     {
-      title:"Saxofón",
+      title:"saxofón",
       img: "assets/images/Sax.jpg",
       icon: "barbell-outline",
       description: "El saxofón es un instrumento de viento-madera utilizado en varios géneros musicales, conocido por su sonido cálido y expresivo."
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -10,38 +10,39 @@ export class LoginPage implements OnInit {
   loginForm: FormGroup;
   validation_messages = {
     email: [
-      { type: "required", message: "El email es obligatio" },
-      { type: "pattern", message: "Debe poner un email valido" }
+      { type: "required", message: "El email es obligatorio" },
+      { type: "pattern", message: "Debe poner un email válido" }
+    ],
+    password: [
+      { type: "required", message: "La contraseña es obligatoria" },
+      { type: "minlength", message: "La contraseña debe tener al menos 6 caracteres" },
+      { type: "maxlength", message: "La contraseña no puede tener más de 8 caracteres" },
+      { type: "pattern", message: "La contraseña debe contener al menos una mayúscula, un número y un carácter alfanumérico" }
     ]
   }
   
   constructor(private formBuilder: FormBuilder) { 
-    this.loginForm = this.formBuilder.group(
-      {
-        email: new FormControl(          
-          "",
-          Validators.compose(
-            [
-              Validators.required,
-              Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]+.[a-zA-Z0-9.-]+$"),
-              Validators.email      
-            ]
-          )        
-        ),
-        password: new FormControl(
-          "",
-          Validators.compose(
-            [
-              Validators.required,
-              Validators.minLength(6)
-            ]
-          )
-        )
-     }
-   )
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(          
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]+.[a-zA-Z0-9.-]+$"),
+          Validators.email      
+        ])
+      ),
+      password: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(8),
+          Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$")
+        ])
+      )
+    });
   }
 
   ngOnInit() {
   }
-
 }
