c50d1fe2741364547e5c4692455a8dbcab0aad4a
Thankord <danedu0727@gmail.com>
2023-07-12 17:45:18 -0500

    Clase 11 Julio 2023

--- src/app/guards/intro.guard.ts
+++ src/app/guards/intro.guard.ts
@@ -9,11 +9,16 @@ import { Router } from '@angular/router';
 })
 export class IntroGuard implements CanActivate {
   constructor(private storage: Storage, private router: Router) {}
-
-  canActivate() {
-    console.log("Paso por seguridad");
-    this.router.navigateByUrl("/intro");
-    return false;
-  }  
+  async canActivate() {
+    const isIntroShowed = await this.storage.get('introShow');
+    console.log(isIntroShowed);
+    if (isIntroShowed){
+      return true;
+    }else{
+      this.router.navigateByUrl('/intro');
+      return false;
+    }
+    
+  }
 
 }
