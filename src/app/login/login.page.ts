dcc61240d230c4108f315d62598351a9d9857181
Thankord <danedu0727@gmail.com>
2023-07-12 21:07:00 -0500

    Clase 12 Julio 2023

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { AuthenticateService } from '../services/authenticate.service';
+import { NavController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
+
 
 @Component({
   selector: 'app-login',
@@ -20,8 +24,14 @@ export class LoginPage implements OnInit {
       { type: "pattern", message: "La contraseña debe contener al menos una mayúscula, un número y un carácter alfanumérico" }
     ]
   }
-  
-  constructor(private formBuilder: FormBuilder) { 
+  errorMessage: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder, 
+    private authService: AuthenticateService,
+    private navCtrl: NavController,
+    private storage: Storage,
+    ) { 
     this.loginForm = this.formBuilder.group({
       email: new FormControl(          
         "",
@@ -44,5 +54,17 @@ export class LoginPage implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  loginUser(credentials: any){
+    console.log(credentials);
+    this.authService.loginUser(credentials).then(res => {
+      this.errorMessage = "";
+      this.storage.set("isUserLoggedIn", true);
+      this.navCtrl.navigateForward("/home");
+    }).catch(err => {
+      this.errorMessage = err;
+      console.log(this.errorMessage);
+    })
   }
 }
